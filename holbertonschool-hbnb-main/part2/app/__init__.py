f2c59477d7c66a0a8745a5e7d6e35377a0572313
matteof7 <matteofoti741@gmail.com>
2025-02-27 15:34:27 +0100

    api

---
+++ holbertonschool-hbnb-main/part2/app/__init__.py
@@ -0,0 +1,24 @@
+from flask import Flask
+from flask_restx import Api
+from app.api.v1.users import api as users_ns
+from app.api.v1.amenities import api as amenities_ns
+
+
+def create_app():
+    app = Flask(__name__)
+    
+    # Créer l'API Flask-RESTX
+    api = Api(
+        app,
+        version='1.0',
+        title='HBnB API',
+        description='HBNB Application API',
+        doc='/api/v1/'
+    )  
+
+    # Enregistrer le namespace users
+    api.add_namespace(users_ns, path='/api/v1/users')
+    
+    api.add_namespace(amenities_ns, path='/api/v1/amenities')
+        
+    return app
